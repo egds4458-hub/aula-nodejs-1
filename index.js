d0b19df4c4840781e6cd988f6e9a08d5ada4a564
Erick Garcia <ericksantos.pf032@academico.ifsul.edu.br>
2026-06-16 15:38:42 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
-//este é meu arquivo principal
+const express = require('express');
+const app = express();
 
-const calc = require('./calc'); //importacao do modulo
-const chalk = require('chalk');
+app.get('/', (req,res)=>{
+    res.send('hello');
+});
 
-
-console.log(chalk.redBright('Este é o meu primeiro programa Node.js'));
-
-const resSoma= calc.soma(2,2);
-const resSub= calc.sub(10,2);
-
-
-console.log(chalk.green( `O resultado da soma é ${resSoma}`) );
-console.log(chalk.blue.bgRed.bold(`O resultado da subtração é ${resSub}`) );  
+app.listen(3001);
